b564ee39d4ddf32781f7c1fb058216aa015a186c
Philipp Mulfinger <info@phil-splash.de>
2023-09-05 18:12:28 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>HELLO</h1>
+    </>
+  )
+}
+
+export default App
